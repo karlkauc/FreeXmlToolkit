4c6e177239dbd29a7bf43dfd6703595e9a946da2
Karl Kauc <karl.kauc@gmail.com>
2022-01-20 08:28:34 +0100

    weiter

--- src/main/java/org/fxt/freexmltoolkit/controls/MyFileChooserTree.java
+++ src/main/java/org/fxt/freexmltoolkit/controls/MyFileChooserTree.java
@@ -1,42 +1,45 @@
 package org.fxt.freexmltoolkit.controls;
 
-import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TreeView;
-import javafx.scene.layout.HBox;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.lang.invoke.MethodHandles;
 
-public class MyFileChooserTree extends HBox {
-    private HBox content;
+public class MyFileChooserTree extends ListView<TreeView<File>> {
+    private ListView<TreeView<File>> content;
 
     private final static Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
-    ObservableList<TreeView<File>> list = FXCollections.emptyObservableList();
+    ObservableList<TreeView<File>> list;
 
     private final static String XML_PATTERN = ".*\\.xml$";
     private final static String XSLT_PATTERN = ".*\\.xslt";
 
-    public void MyFileChooserTree() {
+    public MyFileChooserTree() {
         logger.debug("BIN IM CONSTRUCTOR");
-        content = new HBox();
-        Bindings.bindContent(content.getChildren(), list);
+        list = FXCollections.emptyObservableList();
+        content = new ListView<>();
+        // Bindings.bindContent(content.getItems(), list);
     }
 
     public void setNewItem(String startPath) {
+        logger.debug("set new Item: {}", startPath);
+
         TreeView<File> treeViewXml = new TreeView<>();
         var root1 = new SimpleFileTreeItem(new File(startPath), XML_PATTERN);
         treeViewXml.setRoot(root1);
         treeViewXml.setShowRoot(true);
         // list.add(treeViewXml);
+        content.getItems().add(treeViewXml);
     }
 
 
-    public HBox getContent() {
+    public ListView getContent() {
         return content;
     }
 
