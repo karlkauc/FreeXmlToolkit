c23736d8f9197e809f229ecff5d41a13377f746c
j13hkpb <karl.kauc@erste-am.com>
2022-09-28 16:08:11 +0200

    load pages

--- src/main/java/org/fxt/freexmltoolkit/controller/Main2Controller.java
+++ src/main/java/org/fxt/freexmltoolkit/controller/Main2Controller.java
@@ -3,6 +3,7 @@ package org.fxt.freexmltoolkit.controller;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
@@ -13,6 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -31,7 +33,7 @@ public class Main2Controller {
     StringProperty memoryInfo = new SimpleStringProperty();
 
     @FXML
-    Button exit, xml;
+    Button xslt, xml, xsd, xsdValidation, fop, signature, schematron, help, settings, exit;
 
     @FXML
     void initialize() {
@@ -41,9 +43,11 @@ public class Main2Controller {
             Platform.runLater(() -> version.setText(new Date().toString()));
         }, 1, 2, TimeUnit.SECONDS);
 
+/*
         System.out.println(Runtime.getRuntime().freeMemory() +
                 " \t \t " + Runtime.getRuntime().totalMemory() +
                 " \t \t " + Runtime.getRuntime().maxMemory());
+*/
 
         // oder MemoryMXBean
         exit.setOnAction(e -> System.exit(0));
@@ -51,14 +55,29 @@ public class Main2Controller {
     }
 
     @FXML
-    private void loadXML() {
-        try {
-            Pane newLoadedPane = FXMLLoader.load(getClass().getResource("/pages/tab_xml.fxml"));
-            contentPane.getChildren().add(newLoadedPane);
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
+    public void loadPage(ActionEvent ae) {
+        String pagePath = switch (((Button) ae.getSource()).getId()) {
+            case "xslt" -> null;
+            case "xml" -> "/pages/tab_xml.fxml";
+            case "xsd" -> "/pages/tab_xsd.fxml";
+            case "xsdValidation" -> "/pages/tab_validation.fxml";
+            case "fop" -> "/pages/tab_fop.fxml";
+            case "signature" -> "/pages/tab_signature.fxml";
+            case "schematron" -> null;
+            case "help" -> null;
+            case "settings" -> null;
+            default -> null;
+        };
 
+        if (pagePath != null) {
+            try {
+                Pane newLoadedPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(pagePath)));
+                contentPane.getChildren().clear();
+                contentPane.getChildren().add(newLoadedPane);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
     }
 
 }
