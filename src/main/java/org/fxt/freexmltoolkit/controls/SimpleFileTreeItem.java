03af4053de18553edc3ac543f2e90ede2dee89f9
Karl Kauc <karl.kauc@gmail.com>
2023-03-06 13:26:10 +0100

    copyright

--- src/main/java/org/fxt/freexmltoolkit/controls/SimpleFileTreeItem.java
+++ src/main/java/org/fxt/freexmltoolkit/controls/SimpleFileTreeItem.java
@@ -1,3 +1,21 @@
+/*
+ * FreeXMLToolkit - Universal Toolkit for XML
+ * Copyright (c) 2023.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.fxt.freexmltoolkit.controls;
 
 import javafx.collections.FXCollections;
@@ -86,8 +104,7 @@ public class SimpleFileTreeItem extends TreeItem<File> {
             File[] files;
             if (this.pattern != null) {
                 files = f.listFiles((dir1, name) -> new File(dir1.getAbsolutePath() + "/" + name).isDirectory() || name.matches(this.pattern));
-            }
-            else {
+            } else {
                 files = f.listFiles();
             }
 
