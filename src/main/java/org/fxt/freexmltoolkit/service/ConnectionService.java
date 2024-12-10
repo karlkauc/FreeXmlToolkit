4fb6efa85b152ba301bf36307a0d979310ba251e
j13hkpb <karl.kauc@erste-am.com>
2024-12-10 19:41:13 +0100

    proxy

--- src/main/java/org/fxt/freexmltoolkit/service/ConnectionService.java
+++ src/main/java/org/fxt/freexmltoolkit/service/ConnectionService.java
@@ -1,6 +1,6 @@
 /*
  * FreeXMLToolkit - Universal Toolkit for XML
- * Copyright (c) 2023.
+ * Copyright (c) Karl Kauc 2024.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,9 +18,12 @@
 
 package org.fxt.freexmltoolkit.service;
 
+import java.net.Proxy;
+
 public interface ConnectionService {
     boolean testConnection();
 
     boolean testConnection(String URL);
 
+    Proxy getSystemProxy();
 }
